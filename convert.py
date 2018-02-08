73b9c4d4c055e292c349579fd0ab81ade6a57755
sheenufilms <yousheenu@gmail.com>
2018-02-08 16:42:37 -0500

    Sheenu's files
    The text output for the data file and the screenshot of the game running.

--- convert.py
+++ convert.py
@@ -1,8 +1,7 @@
 import cc_dat_utils
 
-#Part 1
-#Use cc_data_utils.make_cc_data_from_dat() to load pfgd_test.dat
-#print the resulting data
+cc_dat = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
+print(cc_dat)
 
 
 #Part 2
