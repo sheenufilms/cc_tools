c45bb2a9baea4953440232012e7f072195e3d52c
sheenufilms <yousheenu@gmail.com>
2018-02-13 20:25:11 -0500

    "String" error code
    Code that causes "String" errors

--- convert.py
+++ convert.py
@@ -1,4 +1,7 @@
 import cc_dat_utils
+import test_data
+import test_json_utils
+import json
 
 cc_dat = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
 print(cc_dat)
@@ -6,6 +9,15 @@ print(cc_dat)
 
 #Part 2
 input_json_file = "data/test_data.json"
+with open(input_json_file, "r") as reader:
+        game_library_data = json.load(reader)
+print("JSON data:")
+print(game_library_data)
+print("")
+
+gameJason = test_json_utils.make_game_library_from_json(game_library_data)
+print("Game Library loaded from JSON:")
+print(gameJason)
 
 ### Begin Add Code Here ###
 #Open the file specified by input_json_file
--- test_json_utils.py
+++ test_json_utils.py
@@ -2,16 +2,22 @@ import test_data
 import json
 
 #Creates and returns a GameLibrary object(defined in test_data) from loaded json_data
-def make_game_library_from_json( json_data ):
+def make_game_library_from_json(json_data):
     #Initialize a new GameLibrary
-    game_library = test_data.GameLibrary()
+    new_game_library = test_data.GameLibrary()
 
     #Loop through the json_data
+    for game_data in json_data["Games"]:
+        new_game = test_data.Game()
+        new_game.title = game_data["Title"]
+        new_game.title = game_data["Year"]
+        new_game.title = game_data["Platform"]
         #Create a new Game object from the json_data by reading
         #  title
         #  year
         #  platform (which requires reading name and launch_year)
         #Add that Game object to the game_library
+        new_game_library.add_game(new_game)
     #Return the completed game_library
 
-    return game_library
+    return new_game_library
