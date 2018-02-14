e76320b96ad9c2b11e3f27a66c555f83ba7c53d0
sheenufilms <yousheenu@gmail.com>
2018-02-13 22:58:30 -0500

    IT WORKS!
    IT WORKS!!

--- test_json_utils.py
+++ test_json_utils.py
@@ -10,8 +10,13 @@ def make_game_library_from_json(json_data):
     for game_data in json_data["Games"]:
         new_game = test_data.Game()
         new_game.title = game_data["Title"]
-        new_game.title = game_data["Year"]
-        new_game.title = game_data["Platform"]
+        new_game.year = game_data["Year"]
+        platform_data = game_data["Platform"]
+        new_game.platform = test_data.Platform()
+
+        new_game.platform.name = platform_data["Name"]
+        new_game.platform.launch_year = platform_data["Launch Year"]
+
         #Create a new Game object from the json_data by reading
         #  title
         #  year
